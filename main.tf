985a852addbc9109f15d180901c5a89e2d1ca286
veeranki2014 <veeranki2014@gmail.com>
2023-10-28 20:33:42 -0500

    terraform rabbitmq module

---
+++ main.tf
@@ -0,0 +1,46 @@
+
+# Security Groups
+resource "aws_security_group" "main" {
+  name        = "${var.component}-${var.env}-sg"
+  description = "${var.component}-${var.env}-sg"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = var.port
+    to_port     = var.port
+    protocol    = "tcp"
+    cidr_blocks = var.sg_subnet_cidr
+
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags              = merge ({ Name = "${var.component}-${var.env}-lb" }, var.tags )
+}
+
+
+
+resource "aws_lb" "main" {
+  name               = "${var.name}-alb"
+  internal           = var.internal
+  load_balancer_type = var.load_balancer_type
+  security_groups    = [aws_security_group.main.id]
+  subnets            = var.subnets
+
+  //enable_deletion_protection = true
+
+#  access_logs {
+#    bucket  = aws_s3_bucket.lb_logs.id
+#    prefix  = "test-lb"
+#    enabled = true
+#  }
+
+  tags = {
+    Environment = "${var.component}-${var.env}-alb"
+  }
+}
+
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "env" {}
+variable "component" {}
+variable "vpc_id" {}
+variable "port" {
+  default = 80
+}
+variable "sg_subnet_cidr" {}
+variable "name" {}
+variable "internal" {}
+variable "load_balancer_type" {}
+variable "subnets" {}
+variable "tags" {}
