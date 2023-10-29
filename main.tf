dcd5481a105e4724be4654f762ecb063ecc45dda
veeranki2014 <veeranki2014@gmail.com>
2023-10-28 21:09:28 -0500

    terraform rabbitmq module

--- main.tf
+++ main.tf
@@ -44,3 +44,19 @@ resource "aws_lb" "main" {
   }
 }
 
+resource "aws_lb_listener" "main" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = var.port
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Default Error"
+      status_code  = "500"
+    }
+  }
+}
+
